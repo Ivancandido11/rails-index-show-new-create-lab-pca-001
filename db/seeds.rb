99debb981a3324d61eb90725dba410eb7f879420
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-20 10:18:35 -0400

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "SUMMER2020", store: "Macy's")
+Coupon.create(coupon_code: "BACK2SCHOOL", store: "Marshalls")
